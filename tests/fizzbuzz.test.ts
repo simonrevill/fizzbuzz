2b083fdfc286dfb86de9c005adeb104fa1b309f0
Simon Revill <simon.revill@vertumotors.com>
2024-06-10 17:31:38 +0100

    refactor parameterised tests by removing expected parameter and improving test description

--- tests/fizzbuzz.test.ts
+++ tests/fizzbuzz.test.ts
@@ -21,39 +21,30 @@ describe("fizzbuzz tests", () => {
     }
   );
 
-  it.each([
-    [3, "Fizz"],
-    [6, "Fizz"],
-    [9, "Fizz"],
-    [12, "Fizz"],
-  ])('returns string "Fizz" for multiples of three', (number, expected) => {
-    const result = fizzBuzz(number);
-
-    expect(result).toBe(expected);
-  });
-
-  it.each([
-    [5, "Buzz"],
-    [10, "Buzz"],
-    [20, "Buzz"],
-    [25, "Buzz"],
-  ])('returns string "Buzz" for multiples of five', (number, expected) => {
-    const result = fizzBuzz(number);
-
-    expect(result).toBe(expected);
-  });
-
-  it.each([
-    [15, "FizzBuzz"],
-    [30, "FizzBuzz"],
-    [45, "FizzBuzz"],
-    [90, "FizzBuzz"],
-  ])(
-    'returns string "FizzBuzz" for number that is multiple of three and five',
-    (number, expected) => {
+  it.each([[3], [6], [9], [12]])(
+    'returns string "Fizz" for multiples of three - %i',
+    (number) => {
       const result = fizzBuzz(number);
 
-      expect(result).toBe(expected);
+      expect(result).toBe("Fizz");
+    }
+  );
+
+  it.each([[5], [10], [20], [25]])(
+    'returns string "Buzz" for multiples of five - %i',
+    (number) => {
+      const result = fizzBuzz(number);
+
+      expect(result).toBe("Buzz");
+    }
+  );
+
+  it.each([[15], [30], [45], [90]])(
+    'returns string "FizzBuzz" for number that is multiple of three and five - %i',
+    (number) => {
+      const result = fizzBuzz(number);
+
+      expect(result).toBe("FizzBuzz");
     }
   );
 });
