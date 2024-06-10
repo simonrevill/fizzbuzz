3d39282bf5d8037a3c37db55fa0bf0e4b1a22b4c
Simon Revill <simon.revill@vertumotors.com>
2024-06-10 17:18:34 +0100

    adds cases for 'accepts a number as input and returns it as a string' test

--- tests/fizzbuzz.test.ts
+++ tests/fizzbuzz.test.ts
@@ -1,11 +1,19 @@
 import fizzBuzz from "../src/fizzbuzz";
 
 describe("fizzbuzz tests", () => {
-  it("accepts a number as input and returns it as a string", () => {
-    const result = fizzBuzz(1);
+  it.each([
+    [1, "1"],
+    [2, "2"],
+    [4, "4"],
+    [7, "7"],
+  ])(
+    "accepts a number as input and returns it as a string",
+    (number, expected) => {
+      const result = fizzBuzz(number);
 
-    expect(result).toBe("1");
-  });
+      expect(result).toBe(expected);
+    }
+  );
 
   it('returns string "Fizz" for multiples of three', () => {
     const result = fizzBuzz(3);
