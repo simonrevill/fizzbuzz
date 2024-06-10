230a26e7f9d93c7985c02b13a47d09574e4d5104
Simon Revill <simon.revill@vertumotors.com>
2024-06-10 16:21:47 +0100

    refactor: correct previous misunderstanding of requirements!

--- tests/fizzbuzz.test.ts
+++ tests/fizzbuzz.test.ts
@@ -4,6 +4,6 @@ describe("fizzbuzz tests", () => {
   it("accepts a number as input and returns it as a string", () => {
     const result = fizzBuzz(0);
 
-    expect(typeof result).toBe("string");
+    expect(result).toBe("0");
   });
 });
